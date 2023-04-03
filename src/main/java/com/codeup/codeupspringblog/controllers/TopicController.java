d7e8dfd152f7b6027930fc3c50e4b3276a4fbde7
Edgardo Ortiz Jr <edgardo.ortiz.jr11@gmail.com>
2023-04-03 10:09:45 -0500

    progress submission

---
+++ src/main/java/com/codeup/codeupspringblog/controllers/TopicController.java
@@ -0,0 +1,20 @@
+//package com.codeup.codeupspringblog.controllers;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.thymeleaf.expression.Arrays;
+//
+//import java.util.List;
+//
+//@RestController
+//public class TopicController {
+//    @RequestMapping("/topics")
+//    public List<Topic> getAllTopics() {
+//        return
+//                new Topic("spring", "Spring FrameWork", "Spring FrameWork Description")
+//                new Topic("java", "Core Java", "CoreJava Description")
+//                        ;
+//                new Topic("javascript", "Javascript FrameWork", "Javascript Description");
+//
+//    }
+//}
